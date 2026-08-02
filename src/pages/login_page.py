3c221246dcd4dacb488a6b253778f62b6fcd3d9c
Yaroslav Soloninin <solonininy@gmail.com>
2026-08-02 18:17:32 +0300

    test: добавлен тест ui авторизации

---
+++ src/pages/login_page.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page, expect
+
+
+class LoginPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.username = page.get_by_placeholder("Username")
+        self.password = page.get_by_placeholder("Password")
+        self.login_button = page.get_by_role("button", name="Login")
+        self.error_container = page.locator(".error-message-container")
+
+    def login(self, username: str, password: str):
+        self.username.fill(username)
+        self.password.fill(password)
+        self.login_button.click()
+
+    def get_error_message(self) -> str:
+        expect(self.error_container).to_be_visible()
+        return self.error_container.inner_text()
