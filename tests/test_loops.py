ebb342bb1dd33fca8ba9c7409c4488ad16dea599
Yaroslav Soloninin <solonininy@gmail.com>
2026-08-08 21:26:17 +0300

    fix: исправил замечания

--- tests/test_loops.py
+++ tests/test_loops.py
@@ -4,23 +4,27 @@ from src.tools.loops_practice import (
     get_long_words_count,
     load_imitation,
 )
+from tests.test_data.test_data import TestData
 
 
 def test_get_day_name():
-    assert get_day_name(1) == "Понедельник"
-    assert get_day_name(2) == "Вторник"
-    assert get_day_name(7) == "Воскресенье"
-    assert get_day_name(-3) == "Неверный день недели"
+    assert get_day_name(1) == TestData.MONDAY
+    assert get_day_name(2) == TestData.TUESDAY
+    assert get_day_name(7) == TestData.SUNDAY
+    assert get_day_name(-3) == TestData.WORD_DAY_OF_WEEK
 
 
 def test_get_max_value():
-    numbers = list(range(1, 8))
-    assert get_max_value(numbers) == 5
+    numbers = list(range(TestData.LIST_MIN_VALUE, TestData.LIST_MAX_VALUE))
+    assert get_max_value(numbers) == TestData.MAX_NUMBER
 
 
 def test_get_long_words_count():
-    words = [f"str{i}" for i in range(11)]
-    assert get_long_words_count(words, 5) == 1
+    words = [f"str{i}" for i in range(10)]
+    assert (
+        get_long_words_count(words, TestData.LONG_WORD_LENGTH)
+        == TestData.LONG_WORDS_COUNT
+    )
 
 
 def test_load_imitation():
