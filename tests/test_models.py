a912801d01452c362cc34e05cd0cd2ecab0e810a
Yaroslav Soloninin <solonininy@gmail.com>
2026-08-09 16:25:40 +0300

    chore: добавлены проверки на code quality

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,9 @@
 from src.models import Car, Lead, Student
 from src.tools.changer import Changer
-from tests.test_data.test_data import TestData as TD
+from tests.test_data import TestData as TD
 
 
-def test_car():
+def test_car() -> None:
     car1 = Car(TD.CAR1_BRAND, TD.CAR1_MODEL, TD.CAR1_YEAR)
     car2 = Car(TD.CAR2_BRAND, TD.CAR2_MODEL, TD.CAR2_YEAR)
     car3 = Car(TD.CAR3_BRAND, TD.CAR3_MODEL, TD.CAR3_YEAR)
@@ -15,14 +15,14 @@ def test_car():
     assert car3.year == TD.CAR3_YEAR
 
 
-def test_change_lead_name():
+def test_change_lead_name() -> None:
     lead = Lead(TD.LEAD_NAME)
     assert lead.name == TD.LEAD_NAME
     Changer.change_lead_name(lead, TD.NEW_LEAD_NAME)
     assert lead.name == TD.NEW_LEAD_NAME
 
 
-def test_student():
+def test_student() -> None:
     students = [
         Student(TD.STUDENT1_NAME, TD.STUDENT1_AGE, TD.STUDENT1_GRADES),
         Student(TD.STUDENT2_NAME, TD.STUDENT2_AGE, TD.STUDENT2_GRADES),
